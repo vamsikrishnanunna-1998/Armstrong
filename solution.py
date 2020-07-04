c0f9fd0cf93405bf39be1e5f3994482d9bb7b2e1
Irfa Shaik <55692069+Shereen789@users.noreply.github.com>
2020-07-04 10:58:38 +0530

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,7 @@
+# Write a program determine whether the given number is Armstrong number or not.
+# The program should return true or false
+
+def checkArmstrong(num):
+        # Your code goes here
+
+       return False
